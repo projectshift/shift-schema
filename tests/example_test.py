4afb57c258b9b940b89c4b1bb6bfcedf50b929a8
Dmitry Belyakov <db@indee.ru>
2015-01-04 16:53:23 +0300

    Basic tests setup

---
+++ tests/example_test.py
@@ -0,0 +1,13 @@
+from unittest import TestCase, mock
+from nose.plugins.attrib import attr
+
+
+class ExampleTest(TestCase):
+    """
+    Example test
+    This is an example to demonstrate unittests are working
+    """
+
+    def test_example(self):
+        """ Example test """
+        self.assertTrue(True)
