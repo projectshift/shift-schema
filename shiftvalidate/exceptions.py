aa8907694ba721d702f83390bb6254e7085574c3
Dmitry Belyakov <db@indee.ru>
2015-01-06 11:00:43 +0300

    Exceptions

--- shiftvalidate/exceptions.py
+++ shiftvalidate/exceptions.py
@@ -14,3 +14,21 @@ class PropertyExists(ShiftValidateException, ValueError):
     Exception indicates certain property already exists on processor/validator
     """
     pass
+
+
+class InvalidValidator(ShiftValidateException, TypeError):
+    """
+    Invalid validator
+    Raised when trying to add to processor a validator that does not extend
+    from abstract base validator
+    """
+    pass
+
+
+class InvalidFilter(ShiftValidateException, TypeError):
+    """
+    Invalid filter
+    Raised when trying to add to processor a filter that does not extend
+    from abstract base filter
+    """
+    pass
