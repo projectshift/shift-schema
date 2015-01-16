8b33ac30bd17c8011fc752aad4f498c9e27cd205
Dmitry Belyakov <db@indee.ru>
2015-01-16 13:44:22 +0300

    Error object tests

--- tests/result_tests.py
+++ tests/result_tests.py
@@ -1,6 +1,26 @@
 from unittest import TestCase
 from nose.plugins.attrib import attr
 
-@attr('translator')
+from shiftvalidate.result import Error
+
+@attr('result', 'error')
 class ErrorTest(TestCase):
-    pass
+
+    def test_create_error(self):
+        """ Creating an error object """
+        err = Error('message')
+        self.assertIsInstance(err, Error)
+
+    def test_error_evaluates_to_true(self):
+        """ Error with a message evaluates to true"""
+        err = Error('message')
+        self.assertTrue(err)
+        self.assertTrue(err == True)
+        self.assertTrue(err != False)
+
+    def test_empty_error_evaluates_to_false(self):
+        """ Empty error object evaluates to false """
+        err = Error()
+        self.assertFalse(err)
+        self.assertFalse(err == True)
+        self.assertFalse(err != False)
