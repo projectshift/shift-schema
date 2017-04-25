2c7df714785ce42e79ab75a7a31ecce7fc3537a5
Dmitry Belyakov <dmitrybelyakov@gmail.com>
2017-04-25 19:48:02 +0100

    Moving over to twine

---
+++ setup.bak.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+
+version = '0.0.10'
+
+
+# monkey patch os for vagrant hardlinks
+del os.link
+
+
+# prepare config
+config = dict(
+
+    # author
+    author = 'Dmitry Belyakov',
+    author_email='dmitrybelyakov@gmail.com',
+
+    # project meta
+    name='shiftschema',
+    version=version,
+    url = 'https://github.com/projectshift/shift-schema',
+    download_url='https://github.com/projectshift/shift-schema/tarball/'+version,
+    description='Python3 filtering and validation library',
+    keywords=['python3', 'validation', 'filtering', 'schema'],
+
+    # license
+    license='MIT',
+
+    # packages
+    packages=find_packages(exclude=['tests']),
+
+    # dependencies
+    install_requires=[]
+
+)
+
+# finally run the setup
+setup(**config)
+
+# register: ./setup.py register -r pypi
+# build: ./setup.py sdist
+# upload: ./setup.py upload -r pypi
--- setup.py
+++ setup.py
@@ -1,44 +1,100 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import os
 from setuptools import setup, find_packages
 
+# ----------------------------------------------------------------------------
+# Building
+#
+# Create source distribution:
+# ./setup.py sdist
+#
+#
+# Create binary distribution (non-univeral, python 3 only):
+# ./setup.py bdist_wheel --python-tag=py3
+#
+# Register on PyPI:
+# twine register dist/mypkg.whl
+#
+#
+# Upload to PyPI:
+# twine upload dist/*
+#
+# ----------------------------------------------------------------------------
 
+# project version
 version = '0.0.10'
 
+# development status
+# dev_status = '1 - Planning'
+# dev_status = '2 - Pre-Alpha'
+# dev_status = '3 - Alpha'
+dev_status = '4 - Beta'
+# dev_status = '5 - Production/Stable'
+# dev_status = '6 - Mature'
+# dev_status = '7 - Inactive'
+
+# github repository url
+repo = 'https://github.com/projectshift/shift-schema',
+license_type = 'MIT License'
 
 # monkey patch os for vagrant hardlinks
 del os.link
 
-
-# prepare config
-config = dict(
+# run setup
+setup(**dict(
 
     # author
-    author = 'Dmitry Belyakov',
+    author='Dmitry Belyakov',
     author_email='dmitrybelyakov@gmail.com',
 
     # project meta
     name='shiftschema',
     version=version,
-    url = 'https://github.com/projectshift/shift-schema',
-    download_url='https://github.com/projectshift/shift-schema/tarball/'+version,
+    url=repo,
+    download_url=repo + '/archive/v' + version + '.tar.gz',
     description='Python3 filtering and validation library',
-    keywords=['python3', 'validation', 'filtering', 'schema'],
+    keywords=[
+        'python3',
+        'validation',
+        'filtering',
+        'schema',
+        'webapp',
+        'api',
+    ],
 
-    # license
-    license='MIT',
+    # classifiers
+    # see: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
 
-    # packages
-    packages=find_packages(exclude=['tests']),
+        # maturity
+        'Development Status :: ' + dev_status,
 
-    # dependencies
-    install_requires=[]
+        # license
+        'License :: OSI Approved :: ' + license_type,
 
-)
+        # audience
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
 
-# finally run the setup
-setup(**config)
+        # pythons
+        'Programming Language :: Python :: 3',
 
-# register: ./setup.py register -r pypi
-# build: ./setup.py sdist
-# upload: ./setup.py upload -r pypi
+        # categories
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Topic :: Utilities'
+    ],
+
+    # project packages
+    packages=find_packages(exclude=['tests*']),
+
+    # include none-code data files from manifest.in (http://goo.gl/Uf0Yxc)
+    include_package_data=True,
+
+    # project dependencies
+    install_requires=[],
+
+
+    # project license
+    license=license_type
+))
